fc174d60076fcdca96af6ba7fe94740ecb2dbefd
Daedheldir <marcin.bernacki316@gmail.com>
2022-03-10 21:12:11 +0100

    - bmi calculator unit tests

--- Lab1/app/src/main/java/pl/edu/pwr/lab1/i236468/BMICalculator.java
+++ Lab1/app/src/main/java/pl/edu/pwr/lab1/i236468/BMICalculator.java
@@ -1,39 +1,40 @@
 package pl.edu.pwr.lab1.i236468;
 
-public class BMICalculator {
-    enum BMICategories{
-        Underweight3,
-        Underweight2,
-        Underweight1,
-        Normal,
-        Overweight,
-        Obese1,
-        Obese2,
-        Obese3
-    }
-    public static float CalculateBMIValue(float weight, float height){
-        if(height == 0)
-            return Float.MAX_VALUE;
-        return weight / (height * height);
-    }
+public  class BMICalculator {
+	private BMICalculator(){}
+	enum BMICategories{
+		Underweight3,
+		Underweight2,
+		Underweight1,
+		Normal,
+		Overweight,
+		Obese1,
+		Obese2,
+		Obese3
+	}
+	public static float CalculateBMIValue(float weight, float height){
+		if(height == 0)
+			return Float.MAX_VALUE;
+		return weight / (height * height);
+	}
 
-    public static BMICategories GetBMICategory(float bmiIndex){
-        float underweight3Max = 16.0f;
-        float underweight2Max = 16.9f;
-        float underweight1Max = 18.4f;
-        float normalMax = 24.9f;
-        float overweightMax = 29.9f;
-        float obese1Max = 34.9f;
-        float obese2Max = 39.9f;
+	public static BMICategories GetBMICategory(float bmiIndex){
+		float underweight3Max = 16.0f;
+		float underweight2Max = 16.9f;
+		float underweight1Max = 18.4f;
+		float normalMax = 24.9f;
+		float overweightMax = 29.9f;
+		float obese1Max = 34.9f;
+		float obese2Max = 39.9f;
 
-        if(bmiIndex < underweight3Max) return BMICategories.Underweight3;
-        if(bmiIndex < underweight2Max) return BMICategories.Underweight2;
-        if(bmiIndex < underweight1Max) return BMICategories.Underweight1;
-        if(bmiIndex < normalMax) return BMICategories.Normal;
-        if(bmiIndex < overweightMax) return BMICategories.Overweight;
-        if(bmiIndex < obese1Max) return BMICategories.Obese1;
-        if(bmiIndex < obese2Max) return BMICategories.Obese2;
+		if(bmiIndex < underweight3Max) return BMICategories.Underweight3;
+		if(bmiIndex < underweight2Max) return BMICategories.Underweight2;
+		if(bmiIndex < underweight1Max) return BMICategories.Underweight1;
+		if(bmiIndex < normalMax) return BMICategories.Normal;
+		if(bmiIndex < overweightMax) return BMICategories.Overweight;
+		if(bmiIndex < obese1Max) return BMICategories.Obese1;
+		if(bmiIndex < obese2Max) return BMICategories.Obese2;
 
-        return BMICategories.Obese3;
-    }
+		return BMICategories.Obese3;
+	}
 }
--- Lab1/app/src/test/java/pl/edu/pwr/lab1/i236468/BMICalculatorUnitTests.java
+++ Lab1/app/src/test/java/pl/edu/pwr/lab1/i236468/BMICalculatorUnitTests.java
@@ -9,17 +9,28 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+public class BMICalculatorUnitTests {
+	@Test
+	public void bmiCalculationIsCorrect(){
+		assertEquals(2.5f, BMICalculator.CalculateBMIValue(10, 2), 0.01f);
+		assertEquals(Float.MAX_VALUE, BMICalculator.CalculateBMIValue(10, 0), 0.01f);
+	}
 
-    @Test
-    public void bmiCalculationIsCorrect(){assertEquals(2.5f, BMICalculator.CalculateBMIValue(10, 2));}
+	@Test
+	public void bmiCategoriesAreCorrect(){
+		assertEquals(BMICalculator.BMICategories.Underweight3, BMICalculator.GetBMICategory(0.0f));
 
-    @Test
-    public void bmiCategoriesAreCorrect(){
-        assertEquals(BMICalculator.BMICategories.Normal, BMICalculator.GetBMICategory());
-    }
+		assertNotEquals(BMICalculator.BMICategories.Underweight3, BMICalculator.GetBMICategory(16.0f));
+		assertEquals(BMICalculator.BMICategories.Underweight2, BMICalculator.GetBMICategory(16.0f));
+		assertEquals(BMICalculator.BMICategories.Underweight1, BMICalculator.GetBMICategory(17.0f));
+
+		assertEquals(BMICalculator.BMICategories.Normal, BMICalculator.GetBMICategory(18.5f));
+		assertEquals(BMICalculator.BMICategories.Overweight, BMICalculator.GetBMICategory(25.0f));
+
+		assertEquals(BMICalculator.BMICategories.Obese1, BMICalculator.GetBMICategory(30.0f));
+		assertEquals(BMICalculator.BMICategories.Obese2, BMICalculator.GetBMICategory(35.0f));
+		assertEquals(BMICalculator.BMICategories.Obese3, BMICalculator.GetBMICategory(40.0f));
+
+		assertEquals(BMICalculator.BMICategories.Obese3, BMICalculator.GetBMICategory(Float.MAX_VALUE));
+	}
 }
